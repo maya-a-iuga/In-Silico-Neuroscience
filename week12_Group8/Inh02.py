954174dbb4953832a6ee55b47c849fa4334751b2
Maya Iuga <maya_iuga@yahoo.ro>
2022-06-10 12:50:08 +0100

    Adds weekly exercises

---
+++ week12_Group8/Inh02.py
@@ -0,0 +1,80 @@
+from neuron import h
+
+# Load external files & initialize
+h.load_file("stdrun.hoc");
+h.stdinit();
+
+class INH2:
+    def __init__(self):
+        self.build_morphology()
+        self.define_biophysics()
+        
+    def build_morphology(self):
+        """
+        Loads a 3D morphology of the neuron
+        """
+        # Load hoc routines to import 3D morphologies
+        h.load_file('stdlib.hoc')
+        h.load_file("import3d.hoc")
+        cell = h.Import3d_SWC_read() # We have a .swc morphology file
+        #cell = h.Import3d_Neurolucida3() # We have an .asc morphology file
+
+        # Read the file and creates automatically section.connect(parent) statements
+        cell.input('morph_05.swc')
+
+        # Instantiate morphology for simulation and
+        # execute the connect statements and loads the cell into h scope
+        self.importedcell = h.Import3d_GUI(cell,0)
+        self.importedcell.instantiate(None) 
+
+        # Create python lists from the morphology with the different sections: soma, dend, apic and axon
+        self.soma = []
+        self.dend = []
+        self.apic = []
+        self.axon = [] # for the moment we will forget about the axon
+        self.all = []
+        for sec in h.allsec():
+            if 'soma' in sec.name():
+                self.soma.append(sec)
+            if 'dend' in sec.name():
+                self.dend.append(sec)
+            if 'apic' in sec.name():
+                self.apic.append(sec)
+            if 'axon' in sec.name():
+                self.axon.append(sec)
+    
+    def define_biophysics(self):
+        for sec in h.allsec():
+            sec.Ra = 100    # Axial resistance in Ohm * cm
+            sec.cm = 1      # Membrane capacitance in micro Farads / cm^2
+            sec.insert("pas")
+            for seg in sec:
+                seg.pas.g = 0.00003
+                seg.pas.e = -75
+        
+        # Insert passive current in the dendrite
+        for sec in self.soma:
+            sec.insert('hh')
+            for seg in sec:
+                seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+                seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+        
+        if hasattr(self, 'apic'):
+            for sec in self.apic:
+                sec.insert('hh')
+                for seg in sec:
+                    seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+                    seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+        
+        for sec in self.dend:
+            sec.insert('hh')
+            for seg in sec:
+                seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+                seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+                
+        for sec in self.axon:
+            sec.insert('hh')
+            for seg in sec:    
+                seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+                seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+                
